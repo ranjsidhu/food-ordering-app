e5cf9b268c44273b99ade83e86f845444475920c
ranjsidhu <ranjeetsidhu7433@gmail.com>
2023-04-08 21:42:37 +0100

    feat: bump button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import classes from "../../css/HeaderCartButton.module.css";
 import CartIcon from "../../assets/CartIcon";
 
 const HeaderCartButton = (props) => {
+  const [highlightButton, setHighlightButton] = useState(false);
   const cartCtx = useContext(CartContext);
-  const cartQuantity = cartCtx.items.reduce((currentNumber, item) => {
+  const { items } = cartCtx;
+
+  const cartQuantity = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${highlightButton ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setHighlightButton(true);
+
+    const timer = setTimeout(() => {
+      setHighlightButton(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
