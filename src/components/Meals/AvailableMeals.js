bff0688d6145914d60bcdd12744d949eab9396cf
ranjsidhu <ranjeetsidhu7433@gmail.com>
2023-04-08 19:00:52 +0100

    feat: meal item

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,13 +1,22 @@
 import classes from "../../css/AvailableMeals.module.css";
 import dummyMeals from "./dummy-meals";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   const DUMMY_MEALS = dummyMeals.map((meal) => (
-    <li key={meal.id}>{meal.name}</li>
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
   ));
   return (
     <section className={classes.meals}>
-      <ul>{DUMMY_MEALS}</ul>
+      <Card>
+        <ul>{DUMMY_MEALS}</ul>{" "}
+      </Card>
     </section>
   );
 };
---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,7 @@
+import classes from "../../../css/MealItemForm.module.css";
+
+const MealItemForm = (props) => {
+  return <div></div>;
+};
+
+export default MealItemForm;
