6b31ee7f2ae19c6079b7e7bc59a316e2e56e26c8
ranjsidhu <ranjeetsidhu7433@gmail.com>
2023-04-08 21:16:55 +0100

    feat: grouping cart items together

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -5,9 +5,26 @@ const defaultCartState = { items: [], totalAmount: 0 };
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotal =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingItemIndex];
+
+    let updatedItems;
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
     return { items: updatedItems, totalAmount: updatedTotal };
   }
   return defaultCartState;
