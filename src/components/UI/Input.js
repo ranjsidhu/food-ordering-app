d6e80b9c2b5f9482b406be009d4e1dcb9b4aac6e
ranjsidhu <ranjeetsidhu7433@gmail.com>
2023-04-08 19:13:02 +0100

    feat: input

---
+++ src/components/UI/Input.js
@@ -0,0 +1,12 @@
+import classes from "../../css/Input.module.css";
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  );
+};
+
+export default Input;
