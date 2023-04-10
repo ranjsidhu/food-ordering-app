40755d048779544e5f217573ba7aca48b0d0f37f
ranjsidhu <ranjeetsidhu7433@gmail.com>
2023-04-10 18:18:28 +0100

    feat: loading state management

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,9 +5,11 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchMeals = async () => {
+      setIsLoading(true);
       const response = await fetch(
         "https://react-http-43bd9-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
       );
@@ -23,9 +25,18 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   const fetchedMeals = meals.map((meal) => (
     <MealItem
